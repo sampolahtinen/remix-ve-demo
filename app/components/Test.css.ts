af2b236c35d47868ad5ec6bd2da986f159c9e9cc
Sampo Lahtinen <sampo.lahtinen@icloud.com>
2023-04-13 10:08:59 +0200

    basic VE setup

---
+++ app/components/Test.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css";
+
+export const styles = style({
+  background: `black`,
+  color: `green`,
+  fontSize: "20px",
+});
---
+++ app/components/Test.tsx
@@ -0,0 +1,5 @@
+import { styles } from "./Test.css";
+
+export const Test = () => (
+  <span className={styles}>Some text styled with VE</span>
+);
